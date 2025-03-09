522a08dcf5cb636d3f44014837739b508b3c2680
Marcos Vinício Fernandes <marcosviniciohd@hotmail.com>
2025-03-09 16:53:01 -0300

    feat: tipos primitivos, promocao aritmeticas

---
+++ 01-fundamentos-da-linguagem-java/PromocaoAritmetica.java
@@ -0,0 +1,8 @@
+public class PromocaoAritmetica {
+    public static void main(String[] args) {
+        int a = 10;
+        long b = 20;
+        long c = a + b;
+        System.out.println(c);
+    }
+}
---
+++ 01-fundamentos-da-linguagem-java/desafios/DesafioTiposPrimitivos.java
@@ -0,0 +1,11 @@
+package desafios;
+
+public class DesafioTiposPrimitivos {
+    public static void main(String[] args) {
+        double pesoCarga1 = 1033.49;
+        double pesoCarga2 = 849.88;
+
+        int pesoTotal = (int) (pesoCarga1 + pesoCarga2);
+        System.out.println(pesoTotal);
+    }
+}
